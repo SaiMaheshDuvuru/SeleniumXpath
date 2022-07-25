914eca6b4b238e0e19ecded3034a0617e7d786cc
SaiMaheshDuvuru <maheshduvuru14@gmail.com>
2022-07-25 12:22:50 +0530

    Initial Commit

---
+++ Xpath.java
@@ -0,0 +1,35 @@
+package com.seleniumxpath;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Xpath {
+
+public static void main(String[] args) throws Exception {
+		
+ WebDriverManager.chromedriver().setup();
+   WebDriver driver = new ChromeDriver();
+	 driver.manage().window().maximize();
+	   driver.get("https://www.hyrtutorials.com/p/add-padding-to-containers.html");
+	     Thread.sleep(3000);
+		    driver.findElement(By.xpath("//input[@maxlength=10]")).sendKeys("Duvuru");
+		       Thread.sleep(3000);
+		         driver.findElement(By.xpath("//input[@maxlength=15]")).sendKeys("Sai Mahesh");
+		          Thread.sleep(3000);
+		       driver.findElement(By.xpath("//div[@class='container']/input[3]")).sendKeys("maheshmiller10@gmail.com");
+		       Thread.sleep(3000);
+		      driver.findElement(By.xpath("//div[@class='container']/div[2]/input[1]")).sendKeys("maheshmahe");
+		     Thread.sleep(3000);
+		   driver.findElement(By.xpath("//div[@class='container']/input[4]")).sendKeys("maheshmahe");
+		  Thread.sleep(3000);
+	     driver.findElement(By.xpath("//button[text()='Clear']")).click();
+		Thread.sleep(3000);
+	   driver.quit();
+	 
+	   System.out.println("Code Sucessfully Executed");
+	}
+
+}
